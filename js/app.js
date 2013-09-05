5d0953b4ce735014e29d161055d2f87e4be39f96
Alex Navasardyan <alex.navasardyan@yahoo.com>
2013-09-05 15:55:56 -0400

    adding stuff

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 (function() {
   var App = Ember.Application.create();
+
+  App.Router.map(function() {
+    this.route('forms');
+    this.route('actions');
+    this.resource('fruits', function() {
+      this.route('all');
+      this.resource('fruit', { path: '/:fruit_id' });
+    });
+  });
+
+  App.FruitRoute = Ember.Route.extend({
+    model: function() {
+      return {};
+    }
+  });
+
+  //App.FormsRoute - input element and bound properties
+  //App.ActionsRoute - toggle boolean property
+  //App.FruitsRoute && App.FruitsIndexRoute
+  //App.FruitsAllRoute
+  //App.Fruit
+
 })();
